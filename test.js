2fbac06f146e177e6be1eaae30e8b71f856c9cd2
animetosho <animetosho@users.noreply.github.com>
2021-04-20 19:48:09 +1000

    Add bzip test

--- test.js
+++ test.js
@@ -8,35 +8,40 @@ var enc = process.argv[3];
 var data = fs.readFileSync(testfile, enc);
 console.log("Got : " + data.length);
 
-// Set output file
-var fd = fs.openSync(testfile + ".gz", "w", 0644);
-console.log("File opened");
-
-// Create gzip stream
-var gzip = new gzbz2.Gzip;
-gzip.init({level:3});
-
-// Pump data to be gzbz2
-var gzdata = gzip.deflate(data, enc);  // Do this as many times as required
-console.log("Compressed chunk size : " + gzdata.length);
-fs.writeSync(fd, gzdata, 0, gzdata.length, null);
-
-// Get the last bit
-var gzlast = gzip.end();
-console.log("Compressed chunk size: " + gzlast.length);
-fs.writeSync(fd, gzlast, 0, gzlast.length, null);
-fs.closeSync(fd);
-console.log("File closed");
-
-// See if we can uncompress it ok
-var gunzip = new gzbz2.Gunzip;
-gunzip.init({encoding: enc});
-var testdata = fs.readFileSync(testfile + ".gz");
-console.log("Test opened : " + testdata.length);
-var inflated = gunzip.inflate(testdata, enc);
-console.log("GZ.inflate.length: " + inflated.length);
-gunzip.end(); // no return value
-
-if (data.length != inflated.length) {
-    console.log('error! input/output string lengths do not match');
-}
+["G", "B"].forEach(function(type) {
+    console.log("Testing " + type + "zip...");
+    var output = testfile + "." + (type == "G" ? "gz" : "bz2");
+    
+    // Set output file
+    var fd = fs.openSync(output, "w", 0644);
+    console.log("File opened");
+    
+    // Create zip stream
+    var zip = new gzbz2[type + "zip"];
+    zip.init({level:3});
+    
+    // Pump data to be gzbz2
+    var zdata = zip.deflate(data, enc);  // Do this as many times as required
+    console.log("Compressed chunk size : " + zdata.length);
+    fs.writeSync(fd, zdata, 0, zdata.length, null);
+    
+    // Get the last bit
+    var zlast = zip.end();
+    console.log("Compressed chunk size: " + zlast.length);
+    fs.writeSync(fd, zlast, 0, zlast.length, null);
+    fs.closeSync(fd);
+    console.log("File closed");
+    
+    // See if we can uncompress it ok
+    var unzip = new gzbz2[type + "unzip"];
+    unzip.init({encoding: enc});
+    var testdata = fs.readFileSync(output);
+    console.log("Test opened : " + testdata.length);
+    var inflated = unzip.inflate(testdata, enc);
+    console.log(type + "Z.inflate.length: " + inflated.length);
+    unzip.end(); // no return value
+    
+    if (data.length != inflated.length) {
+        console.log('error! input/output string lengths do not match');
+    }
+});
